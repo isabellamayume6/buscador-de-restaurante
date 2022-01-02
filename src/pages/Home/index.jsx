995c6d1403408dc1ac5792ec7bc531c65bab4f22
Isabella Mayume <bellamay@Bella.(none)>
2022-01-02 11:45:35 -0300

    reset de estilo

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { Container } from "./styled";
+const Home = () => <Container>teste</Container>;
+export default Home;
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primaryColor: "#BC9EC1",
+    backgroundColor: "#FDE8E9",
+    textColor: "#000",
+  },
+  fonts: {
+    primaryFont: "Roboto, sans-serif",
+  },
+};
+
+export default theme;
